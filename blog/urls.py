a723f6e90c593f3df757c37463aeb73ea724024b
suloveghimirey <sulove_ghimirey@hotmail.com>
2022-01-26 18:10:37 +0545

    Django Day-4, Update, Delete, Search and messages

--- blog/urls.py
+++ blog/urls.py
@@ -1,8 +1,11 @@
 from django.urls import path
-from .views import homepage, article_details, post_article #importing our homepage function
+from .views import homepage, article_details, post_article, update_article, article_delete, search #importing our homepage function
 
 urlpatterns = [
     path('', homepage, name="home"),
     path('articles-details/<int:pk>/', article_details, name='article_details'),
     path('post-article/', post_article, name='post'),
+    path('post-update/<int:pk>', update_article, name='post_update'),
+    path('post-delete/<int:pk>', article_delete, name='post_delete'),
+    path('search/', search, name='search'),
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,9 +1,11 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, get_object_or_404
 from django.http import HttpResponse
 from django.template import context #to give HTTP RESPONSES
 from .models import Article
 from .forms import ArticlePostForm
 from django.urls import reverse
+
+from django.contrib import messages
 
 #Class based views sajilo hunxa, override 
 #Function based views use
@@ -40,7 +42,9 @@ def post_article(request):
                 title   = data['title'],
                 content = data['content']
             )
+            messages.success(request, "Your article was posted sucessfully.")
             return redirect(reverse('article_details', kwargs={'pk' : article.pk}))
+        messages.error(request, "Your form is invalid.")
 
     form    = ArticlePostForm()
 
@@ -49,6 +53,63 @@ def post_article(request):
     }
 
     return render(request, "blog/post.html", context)
+
+def update_article(request, pk):
+    
+    form_data   = get_object_or_404(Article, id=pk)
+
+    if request.method == "POST":
+        form    = ArticlePostForm(request.POST)
+        if form.is_valid():
+            data    = form.cleaned_data
+            article = form_data
+            article.title = data['title']
+            article.content = data['content']
+            article.save()
+            messages.success(request, "Your article was updated sucessfully.")
+            return redirect(reverse('article_details', kwargs={'pk' : article.pk}))
+        messages.error(request, "Your form is invalid.")
+
+    form    = ArticlePostForm(instance=form_data)
+
+    context = {
+        'form' : form,
+    }
+
+    return render(request, "blog/update.html", context)
+
+def article_delete(request, pk):
+
+    data   = get_object_or_404(Article, id=pk)
+
+    if request.method == "POST":
+        data.delete()
+        messages.success(request, "Your article was deleted sucessfully.")
+        return redirect(reverse('home'))
+
+    context = {
+        'article' : data,
+    }
+
+    return render(request, "blog/delete.html", context)
+
+def search(request):
+
+    if request.method == "POST":
+
+        searched_value  = request.POST['searched']
+
+        if searched_value=="":
+            return redirect(reverse('home'))
+        else:
+            articles = Article.objects.filter(title__contains=searched_value).order_by('-date_created')
+            context = {
+                'searched_title' : searched_value,
+                'posts'          : articles,
+            }
+            return render(request, 'blog/search.html', context)
+
+    return render(request, 'blog/search.html')
 
 # Create your views here.
 # ALL YOUR LOGIC GOES HERE
